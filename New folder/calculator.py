a6c50fc556b9323034a9fdd79d51c6984ceb2de2
Mrunbearable <stephen.wirianata@education.nsw.gov.au>
2025-03-13 14:50:35 +1100

    ye work
    in ter gration

---
+++ New folder/calculator.py
@@ -0,0 +1,4 @@
+import math_operations
+a = int(input("Please enter a number"))
+b = int(input("Please enter another number"))
+print(f"The sum of those two numbers is", math_operations.addnumbers(a,b))
